fe8cb6a0006c6b19bfd2d64f2d4e3dc81388a6b9
Ajinkya Kalaskar <ajinkya5130@gmail.com>
2018-07-14 18:31:07 +0530

    Action sheet Controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-
+//
+//import { SocialSharing } from '@ionic-native/social-sharing';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MusicProvider } from '../providers/music/music';
-
+import {enableProdMode} from '@angular/core';
+enableProdMode();
 @NgModule({
   declarations: [
     MyApp,
@@ -20,6 +22,7 @@ import { MusicProvider } from '../providers/music/music';
     BrowserModule,
     IonicModule.forRoot(MyApp), HttpClientModule,
   ],
+  
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
@@ -27,6 +30,7 @@ import { MusicProvider } from '../providers/music/music';
     ListPage
   ],
   providers: [
+   
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
